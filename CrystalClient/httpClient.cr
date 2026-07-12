3ba052ae42abf3b7a56c5a3333868b5fc3ccb151
MartinAlejandroBaer <134695996+MartinAlejandroBaer@users.noreply.github.com>
2026-07-12 19:24:37 +0200

    Create httpClient.cr

---
+++ CrystalClient/httpClient.cr
@@ -0,0 +1,27 @@
+require "http/client"
+require "json"
+require "time"
+require "base64"
+
+serverIpAddress = "localhost"
+port = 1008
+
+submodelId = "https://example.com/ids/sm/3264_2292_6062_3308"
+base64SubmodelId = Base64.strict_encode(submodelId)
+
+puts "Generated Base64Id: #{base64SubmodelId}"
+
+submodelElementIdShort = "DetectionCounter"
+
+route = "http://#{serverIpAddress}:#{port}/api/v3.0/submodels/#{base64SubmodelId}/submodel-elements/#{submodelElementIdShort}/$value"
+
+puts "Generated Route #{route}"
+
+while true
+    randomInt = Random.new.rand(100) # Generation of a random number to update the property
+    createBody = {"DetectionCounter" => randomInt}
+    puts "[#{Time.local(location: Time::Location.load("Europe/Berlin"))}] Sent value #{createBody.to_json}"
+    # PATCH to REST API Command to update a submodel element property
+    response = HTTP::Client.patch(route, headers: HTTP::Headers{"User-Agent" => "From Crystal"}, body: createBody.to_json )
+    sleep 2.seconds # We introduce a delay of 2 seconds to update the new value
+end
